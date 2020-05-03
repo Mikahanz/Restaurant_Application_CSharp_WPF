514af596d1a880316e36a13396e98a105e702cd4
Michael Hanzel <hanzel.michael@gmail.com>
2020-05-02 20:00:55 -0400

    Add project files.

---
+++ Restaurant_Application_CSharp_WPF/Login.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Restaurant_Application_CSharp_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+
+        String strPin = "";
+        private void btnPin_Click(object sender, RoutedEventArgs e)
+        {
+            Button bt = sender as Button;
+            strPin += bt.Content.ToString();
+            MessageBox.Show(strPin);
+            tbxPin.Password = strPin;
+                                            
+        }
+    }
+}
---
+++ Restaurant_Application_CSharp_WPF/WaiterPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Restaurant_Application_CSharp_WPF
+{
+    /// <summary>
+    /// Interaction logic for WaiterPage.xaml
+    /// </summary>
+    public partial class WaiterPage : Window
+    {
+        public WaiterPage()
+        {
+            InitializeComponent();
+
+            Student student = new Student();
+            List<Student> students = student.generateStudents();
+
+            PhoneNumbers phoneNumber = new PhoneNumbers();
+            List<PhoneNumbers> phoneNumbers = phoneNumber.generatePhoneNumbers();
+
+            //Query syntax
+            var studentsList = from stu in students
+                               where stu.Gender == Gender.Male
+                               select stu;
+
+            dgOrders.ItemsSource = studentsList;
+            dgTables.ItemsSource = studentsList;
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
