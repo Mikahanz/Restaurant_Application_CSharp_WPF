e4cfedbf1d18ca082cae7cf79a6b7ed4f1dad484
Michael Hanzel <hanzel.michael@gmail.com>
2020-05-08 18:17:38 -0400

    Add Files folder

---
+++ Restaurant_Application_CSharp_WPF/Files/RestaurantAppFiles/SQLQueryOfAllTables.sql
@@ -0,0 +1,16 @@
+use MikaHanzRestaurant;
+
+/****** ORDER DETAIL  ******/
+  Select * from OrderDetail;
+
+  /****** ORDER Header  ******/
+  Select * from OrderHeader;
+
+  /****** Employee  ******/
+  Select * from Employee;
+
+  /****** Product  ******/
+  Select * from Product;
+
+  /****** Restaurant Table  ******/
+  Select * from RestaurantTable;
